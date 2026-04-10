619b42ffdc3c10169ad3bb090dbb99238b655639
Vyshnavi Pola <123942712+vysh7@users.noreply.github.com>
2026-04-10 11:26:12 +0530

    schema.sql

--- Online Retail Sales Analysis /schema.sql
+++ Online Retail Sales Analysis /schema.sql
@@ -1,1 +1,27 @@
+CREATE TABLE Customers (
+    customer_id NUMBER PRIMARY KEY,
+    name VARCHAR2(50),
+    city VARCHAR2(50)
+);
 
+CREATE TABLE Products (
+    product_id NUMBER PRIMARY KEY,
+    name VARCHAR2(50),
+    category VARCHAR2(50),
+    price NUMBER
+);
+
+CREATE TABLE Orders (
+    order_id NUMBER PRIMARY KEY,
+    customer_id NUMBER,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Items (
+    order_id NUMBER,
+    product_id NUMBER,
+    quantity NUMBER,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
